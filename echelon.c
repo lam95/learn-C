d8a0eb164a589dbda400e7b8db0a384dec68d0e3
lam95 <lamnguyen31195@gmail.com>
2017-08-06 22:54:15 +0700

    final update pivot func

--- echelon.c
+++ echelon.c
@@ -121,3 +121,45 @@ void interchange(double a[20][20],int n,int r1,int r2)
 	}
 }
 
+void kill (double a[20][20],int j,int n,int m,int r1)
+{
+	int i,k;
+	double c;
+	for(i=r1+1;i<m;i++)
+	{
+		c=a[i][j]/a[r1][j];
+		printf("c[%d]=%.3lf\n",i,c);
+		for (k=j;k<n;k++)
+		a[i][k]=a[i][k]-c*a[r1][k];
+	}
+}
+
+void killup(double a[20][20],int n,int j,int r2)
+{
+	int i,k;
+	double c;
+	for(i=0;i<r2;i++)
+	{
+		c=(double)a[i][j]/a[r2][j];
+		printf("c[%d]=%.3lf\n",i,c);
+		for(k=j;k<n;k++)
+		a[i][k]=a[i][k]-(double)c*a[r2][k];
+	}
+}
+
+void pivot(double a[20][20],int r,int n)
+{
+	int i,j;
+	double c=0;
+	for(i=0;i<=r;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			if(a[i][j]!=0) c=a[i][j];
+			if(a[i][j]!=0) break;
+		}
+		printf("c[%d]=%.3lf\n",i,c);
+		for(j=0;j<n;j++)
+		a[i][j]=a[i][j]/c;
+	}
+}
