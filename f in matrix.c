f900096251888e22ee2fa5196a451f78ab8fc5bc
lam95 <lamnguyen31195@gmail.com>
2017-08-07 23:02:36 +0700

    matrix funcrion v3

--- f in matrix.c
+++ f in matrix.c
@@ -53,3 +53,81 @@ void imt(double a[20][20],int m,int n)
 		printf("\n");
 	}
 }
+void copymatrix(double a[20][20],double b[20][20],int m,int n)
+{
+	int i,j;
+	for (i=0;i<m;i++)
+	for (j=0;j<n;j++)
+	b[i][j]=a[i][j];
+}
+void chuyenvi(double a[20][20],double b[20][20],int m,int n)
+{
+	int i,j;
+	for (i=0;i<n;i++)
+	for (j=0;j<m;j++)
+	b[i][j]=a[j][i];
+}
+void phandu(double a[20][20],double b[20][20],int *m,int *n,int i,int j)
+{
+	int k,p,w;
+	*m=*m-1;
+	*n=*n-1;
+	for(k=0;k<*m;k++)
+	{
+		if(k<i) p=k;
+		else p=k+1;
+		for(w=0;w<*n;w++)
+		{
+			if(w<j) b[k][w]=a[p][w];
+			else b[k][w]=a[p][w+1];
+		}
+	}
+}
+void ktpivot(double a[20][20],int j,int m,int r,int *kt)
+{
+	int i;
+	*kt=0;
+	for(i=r;i<m;i++)
+	{
+		if(a[i][j]!=0) *kt=1;
+		if(*kt==1) break;
+	}
+}
+int minr(double a[20][20],int r,int m,int j)
+{
+	int k,hang;
+	double nn;
+	for(k=r;a[k][j]==0;k++);
+	nn=a[k][j];
+	for(k=r;k<m;k++)
+	{
+		if(a[k][j]!=0)
+		if(fabs(a[k][j])<=nn)
+			hang=k;
+	}
+	return hang;
+}
+void interchange(double a[20][20],int n,int r1,int r2)
+{
+	double x;
+	int j;
+	for(j=0;j<n;j++)
+	{
+		x=a[r1][j];
+		a[r1][j]=a[r2][j];
+		a[r2][j]=x;
+	}
+}
+void kill (double a[20][20],int j,int n,int m,int r)
+{
+	int i,k;
+	double c;
+	for(i=r+1;i<m;i++)
+	{
+		c=(double)a[i][j]/(a[r][j]);
+		printf("c[%d]=%.3lf\n",i,c);
+		for (k=j;k<n;k++)
+		a[i][k]=a[i][k]-c*a[r][k];
+	}
+}
+
